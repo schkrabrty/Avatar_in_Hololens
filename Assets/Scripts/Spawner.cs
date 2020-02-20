7c7a535968c2554a90601524e1dbc7a23a36f5b0
Soumyajit Chakraborty <soumyajitchakraborty20@gmail.com>
2020-02-19 21:47:05 -0600

    Updated Commit

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,7 +5,7 @@ using HoloToolkit.Unity.SpatialMapping;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject AvatarPrefab, HoloLensCamera, TestSphere;
+    public GameObject AvatarPrefab, HoloLensCamera, TestSphere, cube_button;
     private float timer = 0.0f;
     [HideInInspector]
     public bool avatar_instantiated = false;
@@ -24,7 +24,7 @@ public class Spawner : MonoBehaviour
     void Start()
     {
         Vector3 HololensCamera_without_y_position = HoloLensCamera.transform.position - new Vector3(0.0f, HoloLensCamera.transform.position.y, 0.0f);
-
+        cube_button.SetActive(false);
         ten_meters_from_Hololens = HololensCamera_without_y_position + new Vector3(0.0f, 0.0f, /*1.0f*/10.0f);
         twenty_five_meters_from_Hololens = HololensCamera_without_y_position + new Vector3(0.0f, 0.0f, /*2.0f*/25.0f);
         fourty_meters_from_Hololens = HololensCamera_without_y_position + new Vector3(0.0f, 0.0f, /*3.0f*/40.0f);
@@ -61,6 +61,7 @@ public class Spawner : MonoBehaviour
                 Destroy(TestSphere);
                 avatar = Instantiate(AvatarPrefab, test_sphere_position, Quaternion.identity).gameObject;
                 avatar_instantiated = true;
+                cube_button.SetActive(true);
             }
         }
 
